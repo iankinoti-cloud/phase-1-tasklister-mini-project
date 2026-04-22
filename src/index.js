46fb8757f142ee155eb720bba759db396f6b87b1
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-04-22 15:45:56 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Step 2: Select form and add event listener
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  function buildToDo(task) {
+    // Step 4: Create li, set textContent, append to task list
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+  }
+
+  form.addEventListener("submit", (event) => {
+    // Step 2: preventDefault
+    event.preventDefault();
+
+    // Step 3: Use event to gather data by name attribute
+    const task = event.target.elements["new-task-description"].value;
+
+    // Step 3: Call buildToDo with the saved task
+    buildToDo(task);
+  });
 });
